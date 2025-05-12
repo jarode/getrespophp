0e35f7f417048e1fe5df5faf94c6ff0061f47b59
jarode <jarode2@gmail.com>
2025-05-12 13:51:30 +0200

    Update settings.php

--- public/settings.php
+++ public/settings.php
@@ -1,6 +1,6 @@
 <?php
-//define('C_REST_CLIENT_ID','local.5c8bb1b0891cf2.87252039');//Application ID
-//define('C_REST_CLIENT_SECRET','SakeVG5mbRdcQet45UUrt6q72AMTo7fkwXSO7Y5LYFYNCRsA6f');//Application key
+define('C_REST_CLIENT_ID','app.681c8540695902.90549112');//Application ID
+define('C_REST_CLIENT_SECRET','692fMScXA870FM7INaTc0qlFAV9kz51FcxlhDWhe3En7ANvb7S');//Application key
 // or
 //define('C_REST_WEB_HOOK_URL','https://rest-api.bitrix24.com/rest/1/doutwqkjxgc3mgc1/');//url on creat Webhook
 
