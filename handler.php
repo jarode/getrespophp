8782909a274563a07754a8b2c5335b78831155b8
jarode <jarode2@gmail.com>
2025-05-13 20:20:00 +0200

    Update handler.php

--- handler.php
+++ handler.php
@@ -20,12 +20,27 @@ if ($event === 'ONCRMLEADADD') {
 
     $leadId = $fields['ID'] ?? null;
     $lead_result = CRest::call('crm.lead.get', ['ID' => $leadId]);
-
     $lead = $lead_result['result'] ?? [];
 
     $name  = $lead['NAME'] ?? '';
     $email = $lead['EMAIL'][0]['VALUE'] ?? '';
 
+    // 🟨 Jeśli brak e-maila w leadzie, spróbuj pobrać z przypisanego kontaktu
+    if (!$email && isset($lead['CONTACT_ID'])) {
+        $contactId = $lead['CONTACT_ID'];
+        $contact_result = CRest::call('crm.contact.get', ['ID' => $contactId]);
+
+        if (!empty($contact_result['result']['EMAIL'][0]['VALUE'])) {
+            $email = $contact_result['result']['EMAIL'][0]['VALUE'];
+        }
+
+        // Jeśli chcesz, możesz też nadpisać imię, jeśli brakuje
+        if (!$name && !empty($contact_result['result']['NAME'])) {
+            $name = $contact_result['result']['NAME'];
+        }
+    }
+
+    // ✅ Wyślij do GetResponse, jeśli mamy e-mail
     if ($email) {
         $apiKey = '62a96f1wzus8pp7s6o83s233j2to908k';
         $campaignId = 'id0Rg';
@@ -52,13 +67,15 @@ if ($event === 'ONCRMLEADADD') {
         file_put_contents(
             $dir . time() . '_' . rand(1000, 9999) . '_lead.txt',
             var_export([
-                'lead_id'  => $leadId,
-                'name'     => $name,
-                'email'    => $email,
-                'response' => $response,
-                'http'     => $httpCode,
-                'raw'      => $_POST
+                'lead_id'     => $leadId,
+                'name'        => $name,
+                'email'       => $email,
+                'response'    => $response,
+                'http'        => $httpCode,
+                'lead_raw'    => $lead,
+                'contact_raw' => $contact_result['result'] ?? []
             ], true)
         );
     }
 }
+
