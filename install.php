3129783ebd45bd530ad9d3900ad80abea7f7346a
Jarek <jarode2@gmail.com>
2025-05-15 13:12:09 +0200

    Poprawne

--- install.php
+++ install.php
@@ -1,20 +1,7 @@
 <?php
 require_once (__DIR__.'/crest.php');
 
-$result = CRest::installApp();
-if($result['install'])
-{
-	$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
-	$host = $_SERVER['HTTP_HOST'];
-	$path = dirname($_SERVER['REQUEST_URI']);
-	$redirectUrl = $protocol . '://' . $host . $path . '/index.php';
-	header('Location: ' . $redirectUrl);
-	exit;
-}
-else
-{
-	echo 'installation error';
-}
+$install_result = CRest::installApp();
 
 // embedded for placement "placement.php"
 $handlerBackUrl = ($_SERVER['HTTPS'] === 'on' || $_SERVER['SERVER_PORT'] === '443' ? 'https' : 'http') . '://'
@@ -45,10 +32,10 @@ $result = CRest::call(
 
 CRest::setLog(['add' => $result], 'installation');
 
-if($result['rest_only'] === false):?>
+if($install_result['rest_only'] === false):?>
 <head>
 	<script src="//api.bitrix24.com/api/v1/"></script>
-	<?if($result['install'] == true):?>
+	<?if($install_result['install'] == true):?>
 	<script>
 		BX24.init(function(){
 			BX24.installFinish();
@@ -57,7 +44,7 @@ if($result['rest_only'] === false):?>
 	<?endif;?>
 </head>
 <body>
-	<?if($result['install'] == true):?>
+	<?if($install_result['install'] == true):?>
 		installation has been finished
 	<?else:?>
 		installation error
